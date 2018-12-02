fadcc9edda60597044a10204ad825aae326bb644
zhackzey <dypekingzzy@126.com>
2018-12-02 19:09:22 +0800

    test file add test1

---
+++ code/test/test1.c
@@ -0,0 +1,16 @@
+#include "syscall.h"
+int fd1,fd2;
+int result;
+char buffer[20];
+int main()
+{
+    Create("write.txt");
+    fd1 = Open("read.txt");
+    fd2 = Open("write.txt");
+
+    result = Read(buffer, 20, fd1);
+    Write(buffer, result, fd2);
+    Close(fd1);
+    Close(fd2);
+    Halt();
+}
